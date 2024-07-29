e9b0bdc976f918a5ac8f43c4985aea13cc96f757
manucastelnovo <marnutskate@gmail.com>
2024-07-29 10:26:35 -0400

    add main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/presentation/counter/counter_screen.dart';
 
-void main(List<String> args) {
-  runApp(MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,11 +9,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.purple,
+      title: 'Yes No App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: Center(
+          child: FilledButton.tonal(
+            onPressed: () {},
+            child: const Text('Click me'),
+          ),
+        ),
       ),
-      home: const CounterScreen(),
     );
   }
 }
